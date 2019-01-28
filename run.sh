e1a590c9b78474c8e5b58be10b1cef5e38d1d2a8
Jiajie-Mei <jiajie.mei@gmail.com>
2019-01-28 17:24:27 +0800

    more merges

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cp ../LCSTS/pg_select/merged_train.txt train_lcsts.txt
 cp ../UGC/pg_select/merged_train.txt train_ugc.txt
 cat train_lcsts.txt train_ugc.txt > train_merged.txt
-python ./subword_nmt/learn_bpe.py --output_with_freq codec_with_freq.txt  -s 10000 < train_merged.txt > codec.txt
+python ./subword_nmt/learn_bpe.py --output_with_freq codec_with_freq.txt  -s 35000 < train_merged.txt > codec.txt
 ./subword_nmt/get_vocab.py --input train_merged.txt --output vocab.txt
