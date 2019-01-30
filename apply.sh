b7d53bba8913de943b8e862460f1f51fee7a0ae2
Jiajie Mei (mbp2018) <jiajie.mei@gmail.com>
2019-01-31 00:20:38 +0800

    bug fix

--- apply.sh
+++ apply.sh
@@ -8,7 +8,7 @@ do
     mkdir -p ${target_dir}
     for dataset in train val test
     do
-        ./subword_nmt/apply_bpe.py -c codec_${num_merges}.txt \
+        nohup ./subword_nmt/apply_bpe.py -c codec_${num_merges}.txt \
         < ${root_dir}/${data}/pg_select/merged_${dataset}.txt \
         > ${target_dir}/subworded_${dataset}.txt &
     done
