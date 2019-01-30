f417b7386f161c8fed251a8e41a406bf2d566777
Jiajie Mei (mbp2018) <jiajie.mei@gmail.com>
2019-01-30 23:25:51 +0800

    make the script more flexible

--- apply.sh
+++ apply.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+num_merges=${1}
+root_dir=${2}
 
 for data in LCSTS UGC
 do
+    target_dir=${root_dir}/${data}/subworded_${num_merges}
+    mkdir -p ${target_dir}
     for dataset in train val test
     do
-        ./subword_nmt/apply_bpe.py -c codec.txt \
-        < ../${data}/pg_select/merged_${dataset}.txt \
-        > ../${data}/pg_select/subworded_${dataset}.txt &
+        ./subword_nmt/apply_bpe.py -c codec_${num_merges}.txt \
+        < ${root_dir}/${data}/pg_select/merged_${dataset}.txt \
+        > ${target_dir}/subworded_${dataset}.txt &
     done
 done
--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
-cp ../LCSTS/pg_select/merged_train.txt train_lcsts.txt
-cp ../UGC/pg_select/merged_train.txt train_ugc.txt
-cat train_lcsts.txt train_ugc.txt > train_merged.txt
-python ./subword_nmt/learn_bpe.py --output_with_freq codec_with_freq.txt  -s 35000 < train_merged.txt > codec.txt
-./subword_nmt/get_vocab.py --input train_merged.txt --output vocab.txt
+num_merges=${1}
+
+rm -rf train_*
+rm -rf codec*
+rm -rf vocab*
+
+root_dir=/data/meijiajie/data_processing
+train_merged=train_merged.txt
+
+for data in LCSTS UGC
+do
+    cp ${root_dir}/LCSTS/pg_select/merged_train.txt train_${data}.txt
+    cat train_${data}.txt >> ${train_merged}
+done
+
+python ./subword_nmt/learn_bpe.py --output_with_freq codec_with_freq_${num_merges}.txt -s ${num_merges} < ${train_merged} > codec_${num_merges}.txt
+./subword_nmt/get_vocab.py --input ${train_merged} --output vocab_${num_merges}.txt
+python apply.sh ${num_merges} ${root_dir}
