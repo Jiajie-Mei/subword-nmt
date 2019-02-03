f9f25007a858711a1704d88745f21234a458c817
Jiajie-Mei <jiajie.mei@gmail.com>
2019-02-03 12:51:57 +0800

    pure test

---
+++ pure_test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+vanilla=vanilla
+selective=selective
+train=0
+eval1=3
+eval2=4
+
+root_dir=/data/meijiajie/data_processing
+
+cd ${root_dir}
+git clone https://github.com/Jiajie-Mei/pointer-generator.git
+cd pointer-generator && git checkout master
+
+for data in LCSTS UGC
+do
+    for merge in 30000 40000
+    do
+
+        cd ${root_dir}/${data}/subworded_${merge}/
+        cp -r ${root_dir}/pointer-generator .
+        cd pointer-generator && cp -r ../finished_files . && cd ..
+        rm -rf ${vanilla}
+        cp -r pointer-generator ${vanilla} && cd ${vanilla}
+        nohup bash run.sh ${train} ${eval1} ${data} nonselective &
+        cd ..
+        rm -rf ${selective}
+        cp -r pointer-generator ${selective} && cd ${selective}
+        nohup bash run.sh ${train} ${eval1} ${data} selective &
+        cd ..
+        rm -rf pointer-generator
+        let train=${train}+1
+        let train=${train}%3
+        let temp=${eval1} && let eval1=${eval2} && let eval2=${temp}
+    done
+done
+
+cd ${root_dir} && rm -rf pointer-generator
