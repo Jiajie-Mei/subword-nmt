c9857a96a542ba88d90243f3ef1c8f0e80a2a93b
Jiajie Mei (mbp2018) <jiajie.mei@gmail.com>
2019-02-14 00:18:27 +0800

    bug_fix

--- fetch_rouge.py
+++ fetch_rouge.py
@@ -6,7 +6,7 @@ import re
 
 dataset = sys.argv[1]
 
-root_dir = '/data/meijiajie/data_processing/'
+root_dir = 'data/meijiajie/data_processing/'
 list_num_merges = [30000, 40000]
 list_selective = ['vanilla', 'selective']
 
@@ -15,7 +15,7 @@ target_file = open(os.path.join(root_dir, dataset, 'merged_rouge_results.csv'), 
 list_str_metric = ['precision', 'recall', 'f_score']
 list_ngram = ['1', '2', 'l']
 
-header = [' ']
+header = ['\\']
 item2id = dict()
 counter = 0
 for str_metric in list_str_metric:
@@ -26,12 +26,12 @@ for str_metric in list_str_metric:
 
 target_file.write(','.join(header))
 
-pattern = r'rouge_(\d)_(.*?): (\d+\.\d+)'
+pattern = r'rouge_(\w)_(.*?): (\d+\.\d+)'
 
 for num_merges in list_num_merges:
     for selective in list_selective:
-        list_files = glob.glob(root_dir + dataset + '/subworded_%d/%s/log/mei/decode_test*/ROUGE_results.txt')
-        results = ['-'] * 9
+        list_files = glob.glob(root_dir + dataset + '/subworded_%d/%s/log/mei/decode_test*/ROUGE_results.txt' % (num_merges, selective))
+        results = ['-'] * (len(list_str_metric) * len(list_ngram))
         if len(list_files) == 0:
             pass
         else:
@@ -40,9 +40,9 @@ for num_merges in list_num_merges:
             except AssertionError:
                 print(list_files)
                 exit(0)
-            line = ' '.join([line.strip() for line in open(list_files[0], 'r').readlines()])
-            for ngram, str_metric, str_value in re.findall(pattern, line):
+            single_line = ' '.join([line.strip() for line in open(list_files[0], 'r').readlines()])
+            for ngram, str_metric, str_value in re.findall(pattern, single_line):
                 results[item2id[str_metric+ngram]] = str_value
 
         results.insert(0, '%dk,%s' % (num_merges // 1000, selective[:3]))
-        target_file.write('\n' + ','.join(list_str_metric))
+        target_file.write('\n' + ','.join(results))
